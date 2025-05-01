66fdb656ba7e5a6e21fbc055bde5f53faa37f7a3
Reury <reuryaraujo@gmail.com>
2025-05-01 16:27:52 -0300

    commit inicial

---
+++ src/main/java/com/reury/agendasalao/AgendasalaoApplication.java
@@ -0,0 +1,13 @@
+package com.reury.agendasalao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendasalaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendasalaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reury/agendasalao/AgendasalaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reury.agendasalao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendasalaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
